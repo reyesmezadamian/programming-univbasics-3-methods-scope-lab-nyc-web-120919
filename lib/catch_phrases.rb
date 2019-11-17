46a76660ee73b42c5b62e1426392aa389f791bb8
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 01:50:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -15,10 +15,9 @@ def link
   return link_out
 end  
 
-def any_phrase()
-  phrase = "Do A Barrel Roll!\n"
-  puts phrase
-  return phrase
+def any_phrase("Do A Barrel Roll!\n")
+  puts any_phrase
+  return any_phrase
 end  
 
   
